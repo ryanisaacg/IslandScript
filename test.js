c6b9ec051dfd691e46871daf7d2975d0667179d6
Ryan Goldstein <ryanisaacg@gmail.com>
2015-10-25 19:43:55 -0400

    Refactor tests to valid code that tests function, if, and while

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 function test () {
 	if (true) {
 		function doTest () {
-			console.log("This should log.")
+			console.log("This should log numbers from 1 to 10.")
+			var i = 1
+			while (i <= 10) {
+				console.log(i)
+				i += 1
 		}
+	}
 		return doTest
 }
 }
-test.doTest()
-
+test()()
