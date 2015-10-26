df33e0b3f2605a34e6792dedb7f85e0602d36e5e
Ryan Goldstein <ryanisaacg@gmail.com>
2015-10-26 06:32:59 -0400

    Show off new for statement

--- test.js
+++ test.js
@@ -2,10 +2,9 @@ function test () {
 	if (true) {
 		function doTest () {
 			console.log("This should log numbers from 1 to 10.")
-			var i = 0
-			while (i < 10) {
-				console.log(i)
+			for (var i = 0;i < 10;i++) {				console.log(i)
 				i += 1
+				
 		}
 	}
 		return doTest
